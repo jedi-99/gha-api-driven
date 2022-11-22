0155f0a306139801f97283cd1323cec8e8820598
jedi-99 <crajdeep96@gmail.com>
2022-11-22 21:46:46 +0530

    modified tfe_variables

--- tfe_variables.tf
+++ tfe_variables.tf
@@ -1,4 +1,4 @@
-# Value of these variables are provided through Terraform Enterprise variables
+# Value of these variables are provided through variables set in Terraform Enterprise
 
 variable "project_id" {
   type = string
