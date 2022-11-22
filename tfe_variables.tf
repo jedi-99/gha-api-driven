0cf16b9cce4edbc962ce5be0aa757f4ec744197d
jedi-99 <crajdeep96@gmail.com>
2022-11-22 22:20:13 +0530

    removed tfe_variables

--- tfe_variables.tf
+++
@@ -1,7 +0,0 @@
-# Value of these variables are provided through Terraform Enterprise
-
-variable "project_id" {
-  type = string
-  description = "GCP Project ID"
-  default = ""
-}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "project_id" {
+  type = string
+  description = "GCP Project ID"
+  default = "poc-cyber-cloud-sec-1152121"
+}
+
 variable "region" {
   type = string
   description = "Default region to deploy cloud resources"
