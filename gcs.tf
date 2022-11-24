43db30b779b53ddc4e2d2eef34167cd877b0b796
jedi-99 <96409539+jedi-99@users.noreply.github.com>
2022-11-24 18:26:48 +0530

    checkov skip comment added

--- gcs.tf
+++ gcs.tf
@@ -1,4 +1,5 @@
 resource "google_storage_bucket" "sample_bucket" {
+   # checkov:skip=CKV_GCP_62: ADD REASON
    name          = "poc-sample-bucket-1991"
    project       = var.project_id
    location      = var.region
