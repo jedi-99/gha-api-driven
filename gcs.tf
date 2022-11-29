8d3f193881b0d3e37caff1449406e3e76bcb962e
jedi-99 <96409539+jedi-99@users.noreply.github.com>
2022-11-29 20:00:18 +0530

    rename gcs

--- gcs.tf
+++ gcs.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "sample_bucket" {
-   name          = "poc-gcs-prisma-policy-test"
+   name          = "poc-gcs-prisma-policy-test-2"
    project       = var.project_id
    location      = var.region
    force_destroy = true
